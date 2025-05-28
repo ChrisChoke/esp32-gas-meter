5797c0a098b290bfb6b8fa3c89a136a47f191772
ChrisChoke <christoph.schuering@gmail.com>
2025-05-28 13:22:38 +0200

    feat: using new config and gasmeter class to make all the necessary work
    hope i can improve my coding with a bit more object oriented programming.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 # import microdot early to alloc needed memory
 from microdot import Microdot, Response
 from microdot.utemplate import Template
-from mqtt_as import MQTTClient, config
+from mqtt_as import MQTTClient
+from config import Config
+from gasmeter import Gasmeter
 from homeassistant import home_assistant
 
 import gc
 gc.collect()
 
 import ntptime
-import json
 from sys import platform
 import asyncio
 import machine
@@ -17,20 +18,6 @@ import esp
 esp.osdebug(None)
 gc.collect()
 
-def dumpJson(jsonData: dict, fileName: str):
-  """
-  simply dump dictionary to json file
-  """
-  wFile = open(fileName, 'w')
-  wFile.write(json.dumps(jsonData))
-  wFile.close()
-
-def calcGas(gasm3: float) -> float:
-  """
-  calculation m3 to kWh
-  """
-  result = gasm3 * valueJson['brennzahl'] * valueJson['zustandszahl']
-  return result
 
 async def pulse():
   """
@@ -58,23 +45,8 @@ async def up(client):
       await client.up.wait()
       client.up.clear()
       wifi_led(True)
-      await client.publish(f'{topicPub}system/state', 'Online')
+      await client.publish(f'{config["topicPub"]}system/state', 'Online')
       asyncio.create_task(pulse())
-
-async def count_up(m3: float, impulsm3: float) -> float:
-   # function to try to prevent floating point error
-   # convert as string to integer without any calculation and count up
-   imp_str = str(impulsm3)
-   _decimal = imp_str[imp_str.index(".")+1:]
-   decimal = len(_decimal)
-
-   multiplier = 10**decimal
-   str_formatter = f'{{:.{decimal}f}}'
-   _m3_str = str_formatter.format(m3)
-   m3_str = _m3_str.replace(".", "")
-   
-   result = int(int(m3_str) + (impulsm3 * multiplier))
-   return result / multiplier
 
 async def pin_event(client, event):
   global pinReset
@@ -83,30 +55,29 @@ async def pin_event(client, event):
      event.clear()
      pinReset = False
 
-     gasm3 = await count_up(valueJson['gasm3'], valueJson['impulsm3'])
-     gaskWh = calcGas(gasm3)
-     valueJson['gasm3'], valueJson['gaskWh'] = gasm3, gaskWh
-     dumpJson(valueJson,'values.json')
+     gasmeter.do_counting()
+     gasm3, gaskWh = gasmeter.values["gasm3"], gasmeter.values["gaskWh"]
 
-     await client.publish(f'{topicPub}gasm3', str(gasm3))
-     await client.publish(f'{topicPub}gaskWh', str(gaskWh))
+     await client.publish(f'{config["topicPub"]}gasm3', str(gasm3))
+     await client.publish(f'{config["topicPub"]}gaskWh', str(gaskWh))
      print(f'pin: {reedPin.value()}, gaskWh: {gaskWh}, gasm3: {gasm3}')
      asyncio.create_task(pulse())
 
 async def main(client):
   global reedPin, pinReset
   await client.connect()
-  if ntp != None:
+  if config["ntp"] != None:
+    ntptime.host = config["ntp"]
     ntptime.settime()
   print(f'Connected to {config["server"]} MQTT broker')
   reedPin = machine.Pin(config["machinePin"], machine.Pin.IN, None)
   pinEvent = asyncio.Event()
   asyncio.create_task(pin_event(client, pinEvent))
   
-  if homeAssistant:
-     asyncio.create_task(home_assistant(client, topicPub))
-  await client.publish(f'{topicPub}gasm3', str(gasm3))
-  await client.publish(f'{topicPub}gaskWh', str(gaskWh))
+  if config["homeassistant"]:
+     asyncio.create_task(home_assistant(client, config["topicPub"]))
+  await client.publish(f'{config["topicPub"]}gasm3', str(gasmeter.values["gasm3"]))
+  await client.publish(f'{config["topicPub"]}gaskWh', str(gasmeter.values["gaskWh"]))
   pinReset = True
   while True:
     if reedPin.value() == 0 and pinReset:
@@ -126,15 +97,8 @@ def start():
 
 gc.collect()
 
-# merge mqtt_as config with our config.json for defaulting some settings
-configRead = open('config.json').read()
-configJson = json.loads(configRead)
-config.update(configJson)
-
-# many vars i currently dont need to define with default. many defaults come from mqtt_as
-topicPub = config["topicPub"] if "topicPub" in config else 'esp32gas/'
-homeAssistant = config["homeassistant"] if "homeassistant" in config else False
-ntp = config["ntp"] if "ntp" in config else None
+conf = Config("config.json")
+config = conf.config
 
 # set up webrepl if password in config.json
 if config["webreplpw"]:
@@ -164,20 +128,9 @@ if platform == 'esp8266' or platform == 'esp32':
     wifi_led = ledfunc(Pin(0, Pin.OUT, value = 1))  # Red LED for WiFi fail/not ready yet
     blue_led = ledfunc(Pin(2, Pin.OUT, value = 0))  # Message send
 
-try:
-  valuesRead = open('values.json').read()
-except OSError:
-  defaultValues = {"gasm3": 1000.00, "gaskWh": 1000.00, "brennzahl": 11.537 , "zustandszahl": 0.95, "impulsm3": 0.01}
-  dumpJson(defaultValues, 'values.json')
-  valuesRead = open('values.json').read()
+gasmeter = Gasmeter("values.json")
+valueJson = gasmeter.values
 
-valueJson = json.loads(valuesRead)
-gasm3, gaskWh = valueJson['gasm3'], valueJson['gaskWh']
-
-ntptime.host = ntp
-
-config["queue_len"] = 1
-config['will'] = ( f'{topicPub}system/state', 'Offline', False, 0 )
 MQTTClient.DEBUG = True
 client = MQTTClient(config)
 
@@ -195,10 +148,10 @@ async def mainSite(request):
           continue
         else:
           valueJson[key] = float(request.form[key])
-      valueJson['gaskWh'] = calcGas(valueJson['gasm3'])
-      dumpJson(valueJson, 'values.json')
-      await client.publish(f'{topicPub}gasm3', str(valueJson['gasm3']))
-      await client.publish(f'{topicPub}gaskWh', str(valueJson['gaskWh']))
+      gasmeter.calc_power()
+      gasmeter.write_values()
+      await client.publish(f'{config["topicPub"]}gasm3', str(valueJson['gasm3']))
+      await client.publish(f'{config["topicPub"]}gaskWh', str(valueJson['gaskWh']))
       gc.collect()
     elif "reboot" in request.form:
       machine.reset()
