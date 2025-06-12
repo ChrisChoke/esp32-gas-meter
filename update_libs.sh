646f7084434c374067b2a7936ac1b06544610fc0
ChrisChoke <christoph.schuering@gmail.com>
2025-06-12 17:17:05 +0200

    update update_libs.sh

--- update_libs.sh
+++ update_libs.sh
@@ -4,10 +4,12 @@
 cd src/lib
 curl https://codeload.github.com/pfalcon/utemplate/tar.gz/master | tar -xz --strip=1 utemplate-master/utemplate
 
+mkdir -p microdot
 curl https://raw.githubusercontent.com/miguelgrinberg/microdot/main/src/microdot/__init__.py > microdot/__init__.py
 curl https://raw.githubusercontent.com/miguelgrinberg/microdot/main/src/microdot/microdot.py > microdot/microdot.py
 curl https://raw.githubusercontent.com/miguelgrinberg/microdot/main/src/microdot/utemplate.py > microdot/utemplate.py
 
-curl https://raw.githubusercontent.com/peterhinch/micropython-mqtt/master/mqtt_as/mqtt_as.py > mqtt_as.py
+mkdir -p mqtt_as
+curl https://raw.githubusercontent.com/peterhinch/micropython-mqtt/master/mqtt_as/__init__.py > mqtt_as/__init__.py
 
 cd ../..
